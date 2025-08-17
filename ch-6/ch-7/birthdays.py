6107b40075dcce1a73155a9d2b7cdbb327a98fde
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2025-08-17 08:00:18 +0500

    ch-7

---
+++ ch-6/ch-7/birthdays.py
@@ -0,0 +1,19 @@
+birthdays = {'Ali':'Apr 1', 'Bob':'Dec 12', 'Carol':'Mar 4'}
+
+
+while True:
+    print('Enter a name: (Blank to quit)')
+    name = input()
+    if name == '':
+        break
+
+    if name in birthdays:
+        print(f'{birthdays[name]} is the birthday of {name}')
+    else:
+        print("No birthdays exist for " + name)
+        print("What is their birthday?")
+        bday = input()
+        birthdays[name] = bday
+        print('Birthday database updated')
+
+
