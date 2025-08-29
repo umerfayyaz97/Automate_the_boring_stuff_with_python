ee0deac6b83a650d270b167d22153527312a7605
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2025-08-29 22:57:50 +0500

    ch-09

---
+++ ch-8/ch8.py
@@ -0,0 +1,2 @@
+spam = 'SpamSpamBaconSpamEggsSpamSpam'
+print(spam.strip('Spam'))
---
+++ ch-9/isPhoneNumber.py
@@ -0,0 +1,19 @@
+def is_phone_number(text):
+    if len(text) != 12:
+        return False
+    for i in range(0,3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':
+        return False
+    for i in range(4,7):
+        if not text[i].isdecimal():
+            return False
+    if text[7] != '-':
+        return False
+    for i in range(8,12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+print('Is 415-555-4242 a phone number?', is_phone_number('415-555-4232'))
