214c89bfd9b307da1c5d826a9b1f310c5abe7d18
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2025-08-30 22:28:29 +0500

    regexx

--- ch-9/regex.py
+++ ch-9/regex.py
@@ -1,14 +1,17 @@
 import re
 
-# # paranthesis makes groups, acces them by 1 & 2
-# phone_re = re.compile(r'(\d{3})-(\d{3}-\d{4})')
-# mo = phone_re.search("My number is 455-444-1234")
-
-# print(mo.group(2))
-
-
-# no paranthesis makes them non group
-phone_re = re.compile(r'\d{3}-\d{3}-\d{4}')
+# paranthesis makes groups, acces them by 1 & 2
+phone_re = re.compile(r'(\d{3})-(\d{3}-\d{4})')
 mo = phone_re.search("My number is 455-444-1234")
 
 print(mo.group())
+
+#if you want to nreceive all groups in the form of tuple
+# print(mo.groups())
+
+
+# no paranthesis makes them non group
+# phone_re = re.compile(r'\d{3}-\d{3}-\d{4}')
+# mo = phone_re.search("My number is 455-444-1234")
+
+# print(mo.group())
