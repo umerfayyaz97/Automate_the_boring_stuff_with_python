ba311f75b08fc64263e6aaec22d457c1d48fd87d
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2025-08-30 22:47:30 +0500

    comments

--- ch-9/regex.py
+++ ch-9/regex.py
@@ -17,7 +17,7 @@ print(mo.group())
 # print(mo.group())
 
 
-#findall returns a list of strings if there are no groups
+#findall returns a list of strings if there are no groups, finds first occurance only and doesnt overlap
 phone_re = re.compile(r'\d{3}-\d{3}-\d{4}')
 mo = phone_re.findall('Cell: 415-555-9999 Work: 212-555-0000')
 print(mo)
