822f3d27d40128dc99e745c1d0babc3684b98d1e
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2025-08-30 22:56:27 +0500

    []

--- ch-9/regex.py
+++ ch-9/regex.py
@@ -1,10 +1,10 @@
 import re
 
 # paranthesis makes groups, acces them by 1 & 2
-phone_re = re.compile(r'(\d{3})-(\d{3}-\d{4})')
-mo = phone_re.search("My number is 455-444-1234")
+# phone_re = re.compile(r'(\d{3})-(\d{3}-\d{4})')
+# mo = phone_re.search("My number is 455-444-1234")
 
-print(mo.group())
+# print(mo.group())
 
 #if you want to nreceive all groups in the form of tuple
 # print(mo.groups())
@@ -18,7 +18,17 @@ print(mo.group())
 
 
 #findall returns a list of strings if there are no groups, finds first occurance only and doesnt overlap
-phone_re = re.compile(r'\d{3}-\d{3}-\d{4}')
-mo = phone_re.findall('Cell: 415-555-9999 Work: 212-555-0000')
+# phone_re = re.compile(r'\d{3}-\d{3}-\d{4}')
+# mo = phone_re.findall('Cell: 415-555-9999 Work: 212-555-0000')
+# print(mo)
+
+
+#use [] brackets to include all items to look for (similar to a|E|e), ^ for negative.
+vowel_pattern = re.compile(r'[aeiouAEIOU]')
+mo = vowel_pattern.findall('RoboCop eats BABY FOOD')
 print(mo)
 
+#negative with ^ character
+consonant_pattern = re.compile(r'[^aeiouAEIOU]')
+mo = consonant_pattern.findall('RoboCop eats BABY FOOD')
+print(mo) 
