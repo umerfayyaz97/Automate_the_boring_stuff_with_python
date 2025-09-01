37020d39698730455dcba23a9f1e94ab07c8d29a
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2025-09-01 16:52:42 +0500

    email and phone regex

--- ch-9/phoneAndEmail.py
+++ ch-9/phoneAndEmail.py
@@ -15,3 +15,108 @@ email_re = re.compile(r'''([a-zA-z0-9.%+-]+ #usermame
                       (\.[a-zA-Z]{2,4}) #dot-somethins                
                     )''', re.VERBOSE)
 
+
+
+text = '''Skip to main content
+Home
+Shopping cart
+0 Items	Total: $0.00
+Search
+Enter your keywords 
+Catalog
+Merchandise
+Blog
+Early Access
+Write for Us
+About Us
+Contact Us
+Labor Day sale — desktop banner
+Time left
+00
+days
+ 
+20
+hours
+ 
+37
+min
+ 
+54
+sec
+Topics
+Art & Design
+General Computing
+Hacking & Computer Security
+Hardware / DIY
+Kids
+LEGO®
+Linux & BSD
+Manga
+Programming
+Python
+R for All
+Science & Math
+Scratch
+System Administration
+Early Access
+FREE ebook edition with every print book purchased from nostarch.com!
++
+
+EARLY ACCESS lets you read full chapters months before a title's release date!
+User login
+Log in
+Create account
+Contact Us
+Reach Us by Email - email is the best way to reach us
+General inquiries or help with an order: info@nostarch.com
+Bulk orders and special sales questions: sales@nostarch.com
+Academic requests: academic@nostarch.com (Further information)
+Conference and event inquiries: conferences@nostarch.com
+Errata - please send any errata reports to: errata@nostarch.com
+Media requests: media@nostarch.com
+Proposals or editorial inquiries: editors@nostarch.com
+Rights inquiries: rights@nostarch.com (Further information)
+Interested in working with us? 
+View our current job openings
+Physical Address
+No Starch Press Inc
+245 8th Street
+San Francisco, CA 94103
+USA
+
+Mailing Address
+No Starch Press Inc
+329 Primrose Road,  #42
+Burlingame, CA 94010-4093
+USA
+
+Phone: 800.420.7240 or +1 415.863.9900
+Fax: +1 415.863.9950
+
+Reach Us on Social Media
+Twitter Facebook Instagram Linkedin Pinterest
+
+ 
+ 
+
+Navigation
+My account
+Want sweet deals?
+Sign up for our newsletter.
+
+
+About Us  |  Jobs!  |  Sales and Distribution  |  Rights  |  Media  |  Academic Requests  |  Conferences  |  FAQ  |  Contact Us  |  Write for Us  |  Privacy
+Copyright 2025. No Starch Press, Inc '''
+
+
+phone_numers = phone_re.findall(text)
+emails = email_re.findall(text)
+
+clean_numbers = [match[0] for match in phone_numers ]
+# print(clean_numbers)
+
+clean_emails = [match[0] for match in emails]
+# print(clean_emails)
+
+combined = clean_numbers + clean_emails
+print(combined)
