03af7e12f40fb87495338abce2003d5433dde681
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2025-09-03 15:36:15 +0500

    home directory

--- ch-10/ch10.py
+++ ch-10/ch10.py
@@ -1,5 +1,15 @@
 from pathlib import Path
 
 #joining paths
-path = Path('spam' , 'eggs' ,'bacon')
-print(path)
+# path = Path('spam' , 'eggs' ,'bacon')
+# print(path)
+
+#current path
+# import os
+
+# print("CWD")
+# print(Path.cwd())
+
+#home directory
+
+print(Path.home())
