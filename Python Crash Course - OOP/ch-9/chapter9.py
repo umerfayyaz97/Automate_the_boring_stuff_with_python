19db9e1eac2ee707ec8185c51d2069c20ddb3d6c
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2025-09-20 15:07:15 +0500

    chapter 9 crash course OOP

---
+++ Python Crash Course - OOP/ch-9/chapter9.py
@@ -0,0 +1,16 @@
+
+class Dog:
+    """Attempt to model Dogs """
+    def __init__(self, name, age):
+        """Initialize attributies """
+        self.name = name
+        self.age = age
+    
+    def sit(self):
+        """Simulate a dog sitting """
+        print(f"{self.name} is now sitting")
+
+    def roll(self);
+        """Simulate rolling of a dog"""
+        print(f"{self.name} rolled over!")
+
