f4d02a87f4fb9d4b18ab408ebce2756e7d0beb37
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2025-09-20 16:11:43 +0500

    method changing

--- Python Crash Course - OOP/ch-9/chapter9.py
+++ Python Crash Course - OOP/ch-9/chapter9.py
@@ -5,6 +5,7 @@ class Dog:
         """Initialize attributies """
         self.name  = name
         self.age  = age 
+        self.play_streak = 0
     
     def sit(self):
         """Simulate a dog sitting """
@@ -13,9 +14,17 @@ class Dog:
     def roll(self):
         """Simulate rolling of a dog"""
         print(f"{self.name} rolled over!")
+    
+    def update_streak(self, days:int):
+        """Update days of streak"""
+        self.play_streak = days
+        
 
 
 my_dog = Dog("tommy", 2)
 
 print(my_dog.name)
 my_dog.sit()
+
+my_dog.update_streak(5)
+print(my_dog.play_streak)
