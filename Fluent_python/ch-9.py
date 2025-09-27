4a6e1b78a03647c9dcba2806c2cc632b21d464d2
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2025-09-27 17:18:40 +0500

    map fn

--- Fluent_python/ch-9.py
+++ Fluent_python/ch-9.py
@@ -1,7 +1,16 @@
-def factorial(n):
-    """returns n!"""
-    return 1 if n <2 else n * factorial(n-1)
+# def factorial(n):
+#     """returns n!"""
+#     return 1 if n <2 else n * factorial(n-1)
 
 # print(factorial(8))
-print(factorial.__doc__)
+# print(factorial.__doc__)
 # help(factorial)
+
+# fact = factorial
+# print(fact(5))
+
+# m = map(factorial, range(11))
+# print(list(m))
+# print(m)
+
+
