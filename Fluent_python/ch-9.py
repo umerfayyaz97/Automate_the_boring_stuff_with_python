6356726af588a34e53e7fb023e18431fdcd6af1a
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2025-09-28 21:58:20 +0500

    from operator import attrgetter

--- Fluent_python/ch-9.py
+++ Fluent_python/ch-9.py
@@ -65,5 +65,29 @@
 # print(s1)
 
 # -------------------------------------------------
-# getattr
+# getattr (works on objects with nanes attr, named tuples and classes)
+
+# from collections import namedtuple
+
+# metro_data = [
+#  ('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),
+#  ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+#  ('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),
+#  ('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),
+#  ('São Paulo', 'BR', 19.649, (-23.547778, -46.635833)),
+#  ]
+
+# Metropolis = namedtuple('Metropolis',['name', 'cc', 'pop', 'coords'])
+
+# metro_objects = [Metropolis(*data) for data in metro_data ]
+
+# # print(metro_objects)
+# # print(type(metro_objects))
+
+# from operator import attrgetter
+
+# for city in sorted(metro_objects, key=attrgetter('pop')):
+#     print(city)
+
+# -------------------------------------------------------------
 
