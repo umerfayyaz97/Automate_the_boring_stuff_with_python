6b59a4b34a6b3de451f1cf57d189b3262d4d8b71
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2025-11-08 16:53:51 +0500

    Own decorator

--- Fluent_python/ch-9.py
+++ Fluent_python/ch-9.py
@@ -16,30 +16,50 @@
 
 # ------------------------------------
 
-registry = []
+# registry = []
 
-def register(func):
-    print(f'Running register({func})')
-    registry.append(func)
-    return func
+# def register(func):
+#     print(f'Running register({func})')
+#     registry.append(func)
+#     return func
 
-@register
-def f1():
-    print('Running f1()')
+# @register
+# def f1():
+#     print('Running f1()')
 
-@register
-def f2():
-    print('Running f2()')
+# @register
+# def f2():
+#     print('Running f2()')
 
-def f3():
-    print('Running f3()')
+# def f3():
+#     print('Running f3()')
 
-def main():
-    print('Running main()')
-    print('Registry ->', registry)
-    f1()
-    f2()
-    f3()
+# def main():
+#     print('Running main()')
+#     print('Registry ->', registry)
+#     f1()
+#     f2()
+#     f3()
 
-main()
+# main()
 
+# ------------------------------------
+
+def add_five(func):
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs) + 5
+    return wrapper
+
+@add_five
+def sum_two_numbers(a,b):
+    return a+b
+
+result = sum_two_numbers(10,15)
+print(result)
+
+@add_five
+def add_zero(num):
+    return num
+
+result2 = add_zero(10)
+print(result2)
